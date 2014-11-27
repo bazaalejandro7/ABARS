4a20f986852c8232d6d74035fd46eccbecaf8050
Alex <bazaa@my.erau.edu>
2014-11-26 22:04:49 -0500

    no message

---
+++ ABARS/src/Administrator.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * Author: Alejandro Baza
+ * @date 2014-25-11
+ * Description: This file contains the methods that provide an Administrator with privileges
+ * 
+ */
+
+/**
+ * 
+ * @author Alejandro Baza
+ * This method adds an administrator subclass of login that will be used when the login accepts the user as an administrator
+ * @return Set on false, True if the user is an administrator
+ */
+
+//public class Administrator extends Login {
+//	
+//	//Given the user's username and password
+//	public Administrator(String username, String password) {
+//		super(username, password);
+//		
+//	}
+//	//Provides the user with administrator privileges
+//	public boolean AdministratorPrivileges() {
+//
+//		//Set administrator to false
+//		administratorDB = false;
+//		if (administratorDB == true) {
+//			////////Administrator Privileges ON///////////
+//		}
+//	}
+//}
--- ABARS/src/Login.java
+++ ABARS/src/Login.java
@@ -10,9 +10,9 @@ import jxl.read.biff.BiffException;
  * Then use the ValidateLogin method
  */
 public class Login {
-	
+
 	private String username, password;
-	
+
 	/**
 	 * @author Matthew Alpert
 	 * @param username - input username
@@ -23,8 +23,8 @@ public class Login {
 		this.username = username;
 		this.password = password;
 	}
-	
-	
+
+
 	/**
 	 * @author Matthew Alpert
 	 * @return the student associated with the input login information
@@ -37,31 +37,57 @@ public class Login {
 		} catch (BiffException | IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return studentDB.Validate(username, password);
-		
+
 	}
-	
-	
+
+
 	/**
 	 * @author Matthew Alpert
 	 * @return true if the input login information is valid, and false if the input login information is false
 	 * Secondary method
 	 */
 	public boolean ValidateLoginBoolean(){
-		
+
 		StudentDatabase studentDB = null;
 		try {
 			studentDB = new StudentDatabase();
 		} catch (BiffException | IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		if(studentDB.Validate(username, password) != null){
 			return true;
 		}else{
 			return false;
 		}
-			
+
 	}
-}
+
+	/**
+	 * @author Alejandro Baza
+	 * @return false if the user is not an administrator, true if user is an administrator
+	 *  This method checks if the user that logs in has administrative credentials
+	 */
+
+//	public boolean AdministratorLoginCheck() {
+//		
+//		AdministratorDatabase administratorDB = null;
+//		try {
+//			administratorDB = new AdministratorDatabase();
+//		}  catch (BiffException | IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		//Check if the username and password matches an administrator in the database
+//		if(administratorDB.Validate(username, password) != null) {
+//			return true;
+//		}
+//		else
+//			return false;
+	
+	///////////////////////////Needs Administrator Database/////////////////////////
+	}
+
+
