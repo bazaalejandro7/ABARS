d6f28fd01b3bcfeb7278ae54a5fb6565e37c7757
William Merritt <MERRITW1@my.erau.edu>
2014-12-03 12:15:44 -0500

    Login Commit

--- ABARS/src/LoginGUI.java
+++ ABARS/src/LoginGUI.java
@@ -12,21 +12,23 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
+import jxl.read.biff.File;
 
 @SuppressWarnings("serial")
 public class LoginGUI extends JFrame{
-	
+
 	private Starter start;
 	private Student student=null;
 	private JTextField userField=null;
 	private JPasswordField passField=null;
 	private LoginGUI thisLogin=this;
-	
+
 	/**This Class is a JFrame object that prompts the user for username and password
 	 * 
 	 * @author William Merritt
@@ -36,7 +38,7 @@ public class LoginGUI extends JFrame{
 	 */
 	/**Main Constructor: Creates and displays the Frame of the login
 	 * 
-	 * Edited Dec 2 by Courtney Fennell: added image to login screen and changed layout manager
+	 * Edited Dec 2 by Courtney Fennell: added image to login screen
 	 */
 	public LoginGUI(Starter start){
 		this.start=start;
@@ -44,40 +46,30 @@ public class LoginGUI extends JFrame{
 		JButton button=new JButton("Submit");
 		JPanel panel=new JPanel();
 		JPanel superPanel=new JPanel();
-		
+
 		superPanel.setLayout(new BorderLayout());
-<<<<<<< HEAD
-		panel.setLayout(new BoxLayout( panel, BoxLayout.Y_AXIS));
-=======
 		panel.setLayout(new GridLayout(5,1));
->>>>>>> be254b7fc35a3241c8c8d7ebab086eb8867c1577
-		
+
 		userField=new JTextField();
 		passField=new JPasswordField();
 
 		button.addActionListener(new LoginButtonListener());
 		passField.addActionListener(new LoginButtonListener());
-		
-		try{
-			BufferedImage image = ImageIO.read(this.getClass().getResource("erau.jpg"));
-			JLabel icon = new JLabel(new ImageIcon(image));
-			panel.add(icon);
-		}catch(IOException e ){
-			System.out.println("image not available");
-			
-		}
-		
+
+		JLabel image = new JLabel(new ImageIcon("erau.jpg"));
+
+		panel.add(image);
 		panel.add(new JLabel("Enter Username"));
 		panel.add(userField);
 		panel.add(new JLabel("Password"));
 		panel.add(passField);
 		//panel.add(button);
-		
+
 		superPanel.add(panel,BorderLayout.CENTER);
 		superPanel.add(button,BorderLayout.SOUTH);
-		
+
 		this.add(superPanel);
-		this.setSize(200,340);
+		this.setSize(200,180);
 		this.setTitle("Student Login");
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -85,7 +77,7 @@ public class LoginGUI extends JFrame{
 
 	}
 	public void addComponent(JPanel panel,JComponent comp){
-		
+
 	}
 
 	public Student getStudent(){
@@ -101,28 +93,25 @@ public class LoginGUI extends JFrame{
 	private class LoginButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			Student tempStudent;
-			try{
-				Login login=new Login(userField.getText(),new String(passField.getPassword()));
 
-				tempStudent=login.ValidateLoginStudent();
+			Login login=new Login(userField.getText(),new String(passField.getPassword()));
 
-				if(tempStudent!=null){
-					start.startMain(tempStudent);
-					thisLogin.dispose();
-				}
-				else{
-					ImageIcon image = new ImageIcon(getClass().getResource("youshallnotpass.gif"));
-					JOptionPane.showMessageDialog(null, "Incorrect Username/Password", "Invalid Login Attempt", DISPOSE_ON_CLOSE, image);
-				}
+			tempStudent=login.ValidateLoginStudent();
+
+			if(tempStudent!=null){
+				start.startMain(tempStudent);
+				thisLogin.dispose();
 			}
-			catch(IndexOutOfBoundsException exp){
-				ImageIcon image = new ImageIcon(getClass().getResource("youshallnotpass.gif"));
-				JOptionPane.showMessageDialog(null, "Incorrect Username/Password", "Invalid Login Attempt", DISPOSE_ON_CLOSE, image);
+			else{
+				ImageIcon image = new ImageIcon(getClass().getResource("youshallnotpass.jpg"));
+				JOptionPane.showMessageDialog(null, "Incorrect Username/Password", "Invalid Login attempt", DISPOSE_ON_CLOSE, image);
 			}
-
-
-
 		}
-	}
 
+
+
+
+	}
 }
+
+
